8fb1c37a02729d4d2f9e7a1bfa1e81f2bc3809f7
Stefan.Schwartz <stefan.schwartz@anecon.com>
2017-10-14 17:45:56 +0200

    Add project files.

---
+++ HtpcManagerCore/FileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HtpcManagerCore
+{
+    public static class FileHandler
+    {
+        private const string sourceDirPath = @"\\10.0.0.6\media\daten\downloads\complete";
+        private static string destDirPath;
+
+        internal static void MoveAllFiles()
+        {
+            var filePaths = new List<string>(Directory.EnumerateFiles(sourceDirPath, "*.mkv", SearchOption.AllDirectories));
+            Console.WriteLine($"Files found: {filePaths.Count}");
+            filePaths.ForEach(filePath => MoveFile(filePath));
+        }
+
+        private static void MoveFile(string filePath)
+        {
+            Console.WriteLine(filePath);
+            FileInfo actualFile = new FileInfo(filePath);
+
+
+            Console.WriteLine($"Successfully moved \"{actualFile.Name}\" to directory {destDirPath}");
+        }
+
+
+    }
+}
---
+++ HtpcManagerCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HtpcManagerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"HtpcManager Program started on {DateTime.Now.ToString("R")}");
+
+            FileHandler.MoveAllFiles();
+
+
+            Console.Read();
+
+        }
+
+
+    }
+}
